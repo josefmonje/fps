42f725f47a1706dbaef5c903de8aee34ee69b615
josefmonje <josefmonje@gmail.com>
2016-01-07 22:46:10 +0800

    HUD

--- js/app/app.js
+++ js/app/app.js
@@ -21,11 +21,14 @@ angular.module('appCtrls', [])
     this.change = function () {
       return document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element
     }
-    this.available = function () { // http://www.html5rocks.com/en/tutorials/pointerlock/intro/
+    this.available = function () {
       return 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document
     }
-    this.request = function () { // Ask the browser to lock the pointer
+    this.request = function () {
       return element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock
+    }
+    this.exit  = function () {
+      return document.exitPointerLock || document.mozExitPointerLock || document.webkitExitPointerLock
     }
   }
 ])
@@ -55,8 +58,13 @@ angular.module('appCtrls', [])
     $scope.controls = new THREE.PointerLockControls($scope.camera)
     $scope.myPosition = $scope.controls.getObject().position
     $scope.controlsEnabled = false
-    $scope.mouse = {x: 0, y: 0}
     $scope.bullets = []
+
+    var renderer = new THREE.WebGLRenderer()
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+    $scope.renderer = renderer
 
     var onWindowResize = function () {
       $scope.camera.aspect = window.innerWidth / window.innerHeight
@@ -89,12 +97,6 @@ angular.module('appCtrls', [])
       if (event.keyCode == 81) { reset = false }
     }
 
-    var onMouseMove = function (event) {
-      event.preventDefault();
-      $scope.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-      $scope.mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-    }
-
     var startControls = function (document) {
       document.body.requestPointerLock = pointerlock.request()
       if (/Firefox/i.test(navigator.userAgent)) {
@@ -114,12 +116,20 @@ angular.module('appCtrls', [])
       }
     }
 
-    $scope.start = function (document) {
-      document.onclick = $scope.createBullet
-      document.onkeydown = onKeyDown
-      document.onkeyup = onKeyUp
-      document.onmousemove = onMouseMove
-      startControls(document)
+    $scope.start = function (element) {
+      element.onclick = $scope.createBullet
+      element.onkeydown = onKeyDown
+      element.onkeyup = onKeyUp
+      startControls(element)
+    }
+
+    var endControls = function (element) {
+      element.exitPointerLock = pointerlock.exit()
+      element.exitPointerLock()
+    }
+
+    $scope.end = function (element) {
+      endControls(element)
     }
 
     var lockChange = function () {
@@ -131,21 +141,24 @@ angular.module('appCtrls', [])
       }
     }
 
-    $scope.activateControls = function (document, start, error) {
-      document.onclick = start
-      document.onpointerlockchange = lockChange
-      document.onmozpointerlockchange = lockChange
-      document.onwebkitpointerlockchange = lockChange
-      document.onpointererror = error
-      document.onmozpointererror = error
-      document.onwebkitpointererror = error
+    $scope.activateControls = function (element) {
+      var start = function () { $scope.start(element) }
+      var end = function () { $scope.end(element) }
+      element.onpointerlockchange = lockChange
+      element.onmozpointerlockchange = lockChange
+      element.onwebkitpointerlockchange = lockChange
+      element.onclick = start
+      element.onpointererror = end
+      element.onmozpointererror = end
+      element.onwebkitpointererror = end
     }
 
     $scope.init = function (config) {
       $scope.scene = new THREE.Scene()
-      $scope.objects = []
       $scope.prevTime = performance.now()
       $scope.velocity = new THREE.Vector3()
+      $scope.objects = []
+
       var createScene = function () {
         $scope.scene.fog = new THREE.Fog(config.FOG_COLOR, config.FOG_MIN, config.FOG_MAX)
         var light = new THREE.HemisphereLight(config.COLOR_SKY, config.COLOR_GROUND, config.LIGHT_INTENSITY)
@@ -253,12 +266,6 @@ angular.module('appCtrls', [])
       $scope.renderer.render($scope.scene, $scope.camera)
     }
 
-    var renderer = new THREE.WebGLRenderer()
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
-    $scope.renderer = renderer
-
     // objects
     $scope.createBox = function (config, number, x, z) {
       var material = new THREE.MeshPhongMaterial({ specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors })
@@ -269,7 +276,8 @@ angular.module('appCtrls', [])
         geometry.faces[i].vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
         geometry.faces[i].vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
       }
-      for (var i = 0; i < 1; i ++) {
+      var n = number || 1
+      for (var i = 0; i < n; i ++) {
         var mesh = new THREE.Mesh(geometry, material)
         mesh.position.y = Math.floor(Math.random() * 20)
         mesh.position.x = x || Math.floor(
@@ -296,14 +304,15 @@ angular.module('appCtrls', [])
             Math.random() * config.GROUND_SCALE.z * config.TILE_ZOOM)-((config.GROUND_SCALE.z * config.TILE_ZOOM) / 2)
           $scope.scene.add(object)
           $scope.objects.push(object.children[0])
-        })
+         }
+       )
       }
     }
 
     $scope.createBullet = function () {
       var material = new THREE.MeshBasicMaterial({color: 0x333333})
       var geometry = new THREE.SphereGeometry(2, 6, 6)
-      var vector = new THREE.Vector3($scope.mouse.x, $scope.mouse.y, 1)
+      var vector = new THREE.Vector3(0, 0, 1)
       vector.unproject($scope.camera)
       var bullet = new THREE.Mesh(geometry, material)
       bullet.position.set($scope.myPosition.x, $scope.myPosition.y, $scope.myPosition.z)
@@ -312,7 +321,7 @@ angular.module('appCtrls', [])
       bullet.castShadow = true
       $scope.bullets.push(bullet)
       $scope.scene.add(bullet)
-      startControls($document[0]) // added this so all clicks regain pointer lock
+      startControls($document[0]) // so all clicks to screen regain pointer lock
     }
 
     $scope.updateBullets = function (delta) {
@@ -333,6 +342,25 @@ angular.module('appCtrls', [])
 ])
 
 angular.module('rooms', ['ngRoute'])
+.constant('defaultCfgs', {
+  FOG_COLOR: 0xffffff,
+  FOG_MIN: 100,
+  FOG_MAX: 500,
+  COLOR_SKY: 0xeeeeff,
+  COLOR_GROUND: 0x777788,
+  LIGHT_INTENSITY: 0.75,
+  BACKGROUND_COLOR: 0xffffff,
+  GROUND_IMAGE: "./img/terrain/grass.png",
+  RCVSHADOW: true,
+  TILE_REPEAT: 16000,
+  TILE_ZOOM: 1600,
+  GROUND_SCALE: {x: 50, y: 50, z: 50},
+  OBJECTS_SCALE: {x: 0, y: 10, z: 0, multiple: 50},
+  SCALE_X: 50,
+  SCALE_Y: 50,
+  SCALE_Z: 50,
+  TREE_OBJ: "./img/objects/tree/tree.obj",
+})
 .constant('treeCfgs', {
   FOG_COLOR: 0xffffff,
   FOG_MIN: 50,
@@ -384,6 +412,29 @@ angular.module('rooms', ['ngRoute'])
   OBJECTS_SCALE: {x: 10, y: 10, z: 10, multiple: 100},
   TREE_OBJ: "./img/objects/tree/tree.obj",
 })
+.controller('homeCtrl', ['$scope', '$document', 'pointerlock',
+  function ($scope, $document, pointerlock) {
+    $scope.end($document[0])
+  }
+])
+.controller('roomCtrl', ['$routeParams', '$scope', 'pointerlock', '$document', '$controller',
+  function ($routeParams, $scope, pointerlock, $document, $controller) {
+    var controller = $routeParams.room + "Ctrl"
+    var config = $routeParams.room + "Cfgs"
+    var module = angular.injector(['ng', 'rooms'])
+    if (module.has(config)) { config = module.get(config) } // http.get this
+      else config = module.get('defaultCfgs')
+
+    if (pointerlock.available()) {
+      $scope.$on('$routeChangeSuccess', $scope.activateControls($document[0]))
+    }
+
+    $scope.init(config)
+    $scope.animate()
+    try { $controller(controller, { $scope: $scope }) }
+      catch (e) {}
+  }
+])
 .controller('treeCtrl', ['$scope', 'treeCfgs',
   function ($scope, treeCfgs) {
     $scope.createTree(treeCfgs, 1, 1, -100)
@@ -398,45 +449,21 @@ angular.module('rooms', ['ngRoute'])
 .controller('deepforestCtrl', ['$scope', 'deepforestCfgs',
   function ($scope, deepforestCfgs) {
     $scope.createTree(deepforestCfgs, 40)
+    $scope.createBox(deepforestCfgs, 20)
   }
 ])
 .config(['$routeProvider',
   function ($routeProvider) {
     $routeProvider
     .otherwise({ redirectTo: '/' })
-    .when('/', { templateUrl: 'pages/home.html' })
-    .when('/:room*?', {
-      templateUrl: 'pages/components/start.html',
-      controller: ['$routeParams', '$scope', 'pointerlock', '$document', '$controller',
-        function ($routeParams, $scope, pointerlock, $document, $controller) {
-          var blocker = document.getElementById('blocker')
-          var instructions = document.getElementById('instructions')
-
-          var config = $routeParams.room + "Cfgs"
-          var config = angular.injector(['ng', 'rooms']).get(config)
-          var controller = $routeParams.room + "Ctrl"
-
-          if (pointerlock.available()) {
-            var start = function () {
-              blocker.style.display = 'none'
-              instructions.style.display = 'none'
-              $scope.start($document[0])
-            }
-            var error = function () {
-              instructions.style.display = ''
-            }
-            $scope.activateControls($document[0], start, error)
-          } else {
-            instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API'
-          }
-
-          $scope.init(config)
-          $scope.animate()
-          $controller(controller, { $scope: $scope })
-        }
-      ]
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'homeCtrl',
     })
-
+    .when('/:room*?', {
+      template: '',
+      controller: 'roomCtrl',
+    })
   }
 ])
 
